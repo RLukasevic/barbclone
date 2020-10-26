6138579fd0ac3336d56e97fe50a59497ab6c82de
RLukasevic <roblukashevich@gmail.com>
2020-10-26 22:10:58 +0200

    Updates

---
+++ precache-manifest.26c8cdc89c664b3df14b7a3cad50e68d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e954b781e264658777b163df9d6b10",
+    "url": "/barbclone/index.html"
+  },
+  {
+    "revision": "b2e2712560fad178fdfb",
+    "url": "/barbclone/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "ffd5bc70b438578c2090",
+    "url": "/barbclone/static/css/main.74d2f820.chunk.css"
+  },
+  {
+    "revision": "b2e2712560fad178fdfb",
+    "url": "/barbclone/static/js/2.d451dc57.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/barbclone/static/js/2.d451dc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd5bc70b438578c2090",
+    "url": "/barbclone/static/js/main.e382e934.chunk.js"
+  },
+  {
+    "revision": "c3ee8aa7b04068ba13ad",
+    "url": "/barbclone/static/js/runtime-main.4b936ddf.js"
+  },
+  {
+    "revision": "0faa78e0cc48f3e00706a8894598063f",
+    "url": "/barbclone/static/media/greenTick.0faa78e0.png"
+  }
+]);
